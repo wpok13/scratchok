ee043ad279d5a99aaac4823534f6ed27cd47303d
wpok13 <65955373+wpok13@users.noreply.github.com>
2021-01-02 07:56:57 +0200

    WordPress Theme Development Tutorial. Part #2
    WordPress Theme Development Tutorial. Part #2

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="styles.css">
-</head>
-<body>
+<?php 
+    get_header( );
+?>
     <?php
     if ( have_posts() ) : 
         while ( have_posts() ) : the_post();
@@ -16,6 +10,8 @@
         _e( 'Sorry, no posts matched your criteria.', 'textdomain' );
     endif;
     ?>
-</body>
-</html>
+<?php 
+    get_sidebar( );
+    get_footer( );
+?>
 
